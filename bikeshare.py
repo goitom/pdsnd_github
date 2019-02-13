9b092ba7daf88a0cd5fb0c9429e0ed83dcc6cd99
Goitom <senay.goitom@dnvgl.com>
2019-02-13 12:17:48 -0600

    explain lists and dictionaries definitions at begininning of code

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,10 @@ import time
 import pandas as pd
 import numpy as np
 
+'''
+Define lists and dictionaries containing file names and valid values 
+for filtering
+'''
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
